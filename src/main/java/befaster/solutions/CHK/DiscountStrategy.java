28e0bb3cc8043110f62f3d86ee14864018de28d6
sbx_user1051 <sbx_user1051@169.254.17.29>
2023-10-08 09:36:08 +0000

    Sun Oct 08 09:36:08 UTC 2023

--- src/main/java/befaster/solutions/CHK/DiscountStrategy.java
+++ src/main/java/befaster/solutions/CHK/DiscountStrategy.java
@@ -3,5 +3,20 @@ package befaster.solutions.CHK;
 public class DiscountStrategy {
   Item item;
   int numOfItems;
-  
+  int discountedPrice;
+
+
+
+  public Item getItem() {
+    return item;
+  }
+
+  public int getNumOfItems() {
+    return numOfItems;
+  }
+
+  public int getDiscountedPrice() {
+    return discountedPrice;
+  }
 }
+
--- src/main/java/befaster/solutions/CHK/FreeItemStrategy.java
+++ src/main/java/befaster/solutions/CHK/FreeItemStrategy.java
@@ -1,5 +1,26 @@
 package befaster.solutions.CHK;
 
 public class FreeItemStrategy {
+  Item item;
+  int numOfItems;
+  Item freeItem;
 
+  public FreeItemStrategy(Item item, int numOfItems, Item freeItem) {
+    this.item = item;
+    this.numOfItems = numOfItems;
+    this.freeItem = freeItem;
+  }
+
+  public Item getItem() {
+    return item;
+  }
+
+  public int getNumOfItems() {
+    return numOfItems;
+  }
+
+  public Item getFreeItem() {
+    return freeItem;
+  }
 }
+
