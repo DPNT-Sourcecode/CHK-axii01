a1dd0e53321c5d2bf2b3beb21c4d9fbeb6d1d80d
sbx_user1051 <sbx_user1051@169.254.21.161>
2023-10-08 13:14:47 +0000

    Sun Oct 08 13:14:47 UTC 2023

--- src/main/java/befaster/solutions/CHK/NormalPriceStrategy.java
+++ src/main/java/befaster/solutions/CHK/NormalPriceStrategy.java
@@ -13,6 +13,26 @@ public class NormalPriceStrategy implements Strategy {
     itemToPrice.put("D", 15);
     itemToPrice.put("E", 40);
     itemToPrice.put("F", 10);
+    itemToPrice.put("G", 20);
+    itemToPrice.put("H", 10);
+    itemToPrice.put("I", 35);
+    itemToPrice.put("J", 60);
+    itemToPrice.put("K", 80);
+    itemToPrice.put("L", 90);
+    itemToPrice.put("M", 15);
+    itemToPrice.put("N", 40);
+    itemToPrice.put("O", 10);
+    itemToPrice.put("P", 50);
+    itemToPrice.put("Q", 30);
+    itemToPrice.put("R", 50);
+    itemToPrice.put("S", 30);
+    itemToPrice.put("T", 20);
+    itemToPrice.put("U", 40);
+    itemToPrice.put("V", 50);
+    itemToPrice.put("W", 20);
+    itemToPrice.put("X", 90);
+    itemToPrice.put("Y", 10);
+    itemToPrice.put("Z", 50);
   }
 
   @Override public int apply(HashMap<String, Integer> itemToCount) {
@@ -25,3 +45,4 @@ public class NormalPriceStrategy implements Strategy {
     return sum;
   }
 }
+
