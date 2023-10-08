fc4be969ec299ef2035bb8cbe9a301a1af0baafe
sbx_user1051 <sbx_user1051@169.254.72.93>
2023-10-08 17:56:12 +0000

    Sun Oct 08 17:56:12 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -4,23 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 
 public class CheckoutSolution {
-  private static final List<Strategy> allStrategies = List.of(
-      new FreeItemStrategy("E", 2, "B"),
-      new FreeItemStrategy("N", 3, "M"),
-      new FreeItemStrategy("R", 3, "Q"),
-      new DiscountStrategy("A", 5, 200),
-      new DiscountStrategy("A", 3, 130),
-      new DiscountStrategy("B", 2, 45),
-      new DiscountStrategy("F", 3, 20),
-      new DiscountStrategy("U", 4, 120),
-      new DiscountStrategy("H", 10, 80),
-      new DiscountStrategy("H", 5, 45),
-      new DiscountStrategy("K", 2, 150),
-      new DiscountStrategy("P", 5, 200),
-      new DiscountStrategy("Q", 3, 80),
-      new DiscountStrategy("V", 3, 130),
-      new DiscountStrategy("V", 2, 90),
-      new NormalPriceStrategy());
+  private static final List<Strategy> allStrategies = List.of(new FreeItemStrategy("E", 2, "B"),
+      new FreeItemStrategy("N", 3, "M"), new FreeItemStrategy("R", 3, "Q"), new DiscountStrategy("A", 5, 200),
+      new DiscountStrategy("A", 3, 130), new DiscountStrategy("B", 2, 45), new DiscountStrategy("F", 3, 20),
+      new DiscountStrategy("U", 4, 120), new DiscountStrategy("H", 10, 80), new DiscountStrategy("H", 5, 45),
+      new DiscountStrategy("K", 2, 150), new DiscountStrategy("P", 5, 200), new DiscountStrategy("Q", 3, 80),
+      new DiscountStrategy("V", 3, 130), new DiscountStrategy("V", 2, 90),
+      new GroupOfferStrategy(List.of("S", "T", "X", "Y", "Z"), 3, 45), new NormalPriceStrategy());
 
   public Integer checkout(String skus) {
     StringBuilder str = new StringBuilder(skus);
@@ -41,4 +31,5 @@ public class CheckoutSolution {
     return allStrategies.stream().mapToInt(strategy -> strategy.apply(itemToCount)).sum();
   }
 }
+
 
--- src/main/java/befaster/solutions/CHK/GroupOfferStrategy.java
+++ src/main/java/befaster/solutions/CHK/GroupOfferStrategy.java
@@ -18,18 +18,28 @@ public class GroupOfferStrategy implements Strategy {
   @Override public int apply(HashMap<String, Integer> itemToCount) {
     int totalItems = 0;
     int price = 0;
-    for (Map.Entry<String, Integer> entry : itemToCount.entrySet()) {
-      if (itemNames.contains(entry.getKey())) {
-        totalItems += entry.getValue();
+    for (String item : itemNames) {
+      if (itemToCount.get(item) != null) {
+        totalItems += itemToCount.get(item);
       }
     }
     if (totalItems >= numOfItems) {
       price = totalItems / numOfItems * discountedPrice;
       int countToRemove = totalItems - totalItems % numOfItems;
-      
+      for (String item : itemNames) {
+        if (countToRemove > 0) {
+          Integer count = itemToCount.get(item);
+          if (count != null && count > 0) {
+            itemToCount.put(item, count - 1);
+            countToRemove--;
+          }
+        } else
+          break;
+      }
     }
     return price;
   }
 }
+
 
 
