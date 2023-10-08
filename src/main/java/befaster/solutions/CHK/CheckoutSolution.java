ed6c1e35598f94e461667ac6748720ea50848035
sbx_user1051 <sbx_user1051@169.254.21.161>
2023-10-08 13:22:00 +0000

    Sun Oct 08 13:22:00 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,13 +5,32 @@ import java.util.List;
 
 public class CheckoutSolution {
 
-  private static final DiscountStrategy discountStrategy1 = new DiscountStrategy("A", 5, 200);
-  private static final DiscountStrategy discountStrategy2 = new DiscountStrategy("A", 3, 130);
-  private static final DiscountStrategy discountStrategy3 = new DiscountStrategy("B", 2, 45);
-  private static final DiscountStrategy discountStrategy4 = new DiscountStrategy("F", 3, 20);
   private static final FreeItemStrategy freeItemStrategy1 = new FreeItemStrategy("E", 2, "B");
-  private static final List<Strategy> allStrategies = List.of(freeItemStrategy1, discountStrategy1, discountStrategy2,
-      discountStrategy3, discountStrategy4, new NormalPriceStrategy());
+  private static final FreeItemStrategy freeItemStrategy2 = new FreeItemStrategy("N", 3, "M");
+  private static final FreeItemStrategy freeItemStrategy3 = new FreeItemStrategy("R", 3, "Q");
+
+//  private static final DiscountStrategy discountStrategy1 = new DiscountStrategy("A", 5, 200)
+//  private static final DiscountStrategy discountStrategy2 = new DiscountStrategy("A", 3, 130)
+//  private static final DiscountStrategy discountStrategy3 = new DiscountStrategy("B", 2, 45)
+//  private static final DiscountStrategy discountStrategy4 = new DiscountStrategy("F", 3, 20)
+//  private static final DiscountStrategy discountStrategy5 = new DiscountStrategy("U", 3, 80)
+  private static final List<Strategy> allStrategies = List.of(
+      new FreeItemStrategy("E", 2, "B"),
+      new FreeItemStrategy("N", 3, "M"),
+      new FreeItemStrategy("R", 3, "Q"),
+      new DiscountStrategy("A", 5, 200),
+      new DiscountStrategy("A", 3, 130),
+      new DiscountStrategy("B", 2, 45),
+      new DiscountStrategy("F", 3, 20),
+      new DiscountStrategy("U", 3, 80),
+      new DiscountStrategy("H", 5, 45),
+      new DiscountStrategy("H", 10, 80),
+      new DiscountStrategy("K", 2, 150),
+      new DiscountStrategy("P", 5, 200),
+      new DiscountStrategy("Q", 3, 80),
+      new DiscountStrategy("V", 3, 130),
+      new DiscountStrategy("V", 2, 90),
+      new NormalPriceStrategy());
 
   public Integer checkout(String skus) {
     StringBuilder str = new StringBuilder(skus);
@@ -32,5 +51,6 @@ public class CheckoutSolution {
     return allStrategies.stream().mapToInt(strategy -> strategy.apply(itemToCount)).sum();
   }
 }
+
 
 
