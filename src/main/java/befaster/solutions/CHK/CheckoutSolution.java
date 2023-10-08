5ac85d961bb847de9f5109f7bd4f48a2f8e0e520
sbx_user1051 <sbx_user1051@169.254.21.161>
2023-10-08 13:02:47 +0000

    Sun Oct 08 13:02:47 UTC 2023

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -10,28 +10,27 @@ public class CheckoutSolution {
   private static final DiscountStrategy discountStrategy3 = new DiscountStrategy("B", 2, 45);
   private static final DiscountStrategy discountStrategy4 = new DiscountStrategy("F", 3, 20);
   private static final FreeItemStrategy freeItemStrategy1 = new FreeItemStrategy("E", 2, "B");
-//  private static final FreeItemStrategy freeItemStrategy2 = new FreeItemStrategy("F", 2, "F");
-  private static final List<Strategy> allStrategies = List.of(freeItemStrategy1, discountStrategy1,
-      discountStrategy2, discountStrategy3, discountStrategy4, new NormalPriceStrategy());
+  private static final List<Strategy> allStrategies = List.of(freeItemStrategy1, discountStrategy1, discountStrategy2,
+      discountStrategy3, discountStrategy4, new NormalPriceStrategy());
 
   public Integer checkout(String skus) {
     StringBuilder str = new StringBuilder(skus);
     HashMap<String, Integer> itemToCount = new HashMap<>();
-    itemToCount.put("A", 0);
-    itemToCount.put("B", 0);
-    itemToCount.put("C", 0);
-    itemToCount.put("D", 0);
-    itemToCount.put("E", 0);
-    itemToCount.put("F", 0);
     for (int i = 0; i < skus.length(); i++) {
-      String currentItem = String.valueOf(str.charAt(i));
-      if (itemToCount.get(currentItem) == null) {
+      char currentChar = str.charAt(i);
+      if (!Character.isUpperCase(currentChar) || !Character.isLetter(currentChar)) {
         return -1;
       }
-      itemToCount.put(currentItem, itemToCount.get(currentItem) + 1);
+      String currentItem = String.valueOf(currentChar);
+      if (itemToCount.containsKey(currentItem)) {
+        itemToCount.put(currentItem, itemToCount.get(currentItem) + 1);
+      } else {
+        itemToCount.put(currentItem, 1);
+      }
     }
 
     return allStrategies.stream().mapToInt(strategy -> strategy.apply(itemToCount)).sum();
   }
 }
+
 
